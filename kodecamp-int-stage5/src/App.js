6271e08bd33c94d5bec0c858751f5088dd6da5c9
chuma6619 <chuma6619@gmail.com>
2022-05-13 22:23:35 +0100

    task 5

--- kodecamp-int-stage5/src/App.js
+++ kodecamp-int-stage5/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {HashRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Aboutpage from './Components/Aboutpage';
 import Contactpage from './Components/Contactpage';
 import Navbar from './Components/Navbar';
@@ -8,18 +8,18 @@ import Details from './Components/Details';
 
 function App() {
   return (
-    <HashRouter  >
+    <BrowserRouter basename={window.location.pathname || ''} >
        <div>
          <Navbar />
          <Routes>
-           <Route path='/' element={<Datapage />} />
+           <Route path='/' exact element={<Datapage />} />
            <Route path='/about' element={<Aboutpage />} />
            <Route path='/contact' element={<Contactpage />} />
            <Route path='/details/:id' element={<Details />} />
          </Routes>
       </div>
     
-    </HashRouter>
+    </BrowserRouter>
  
   );
 }
