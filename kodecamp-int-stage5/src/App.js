16b0ab44d6ba88022d8845f2bf0d7562f9a4d9b9
Chuma Chibueze <102015137+chumachibueze@users.noreply.github.com>
2022-05-14 04:59:34 +0100

    Update App.js

--- kodecamp-int-stage5/src/App.js
+++ kodecamp-int-stage5/src/App.js
@@ -8,11 +8,11 @@ import Details from './Components/Details';
 
 function App() {
   return (
-    <BrowserRouter  >
+    <BrowserRouter basename={window.location.pathname || ''} >
        <div>
          <Navbar />
          <Routes>
-           <Route path='/' exact element={<Datapage />} />
+           <Route path='/kodecamp-int-stage5' element={<Datapage />} />
            <Route path='/about' element={<Aboutpage />} />
            <Route path='/contact' element={<Contactpage />} />
            <Route path='/details/:id' element={<Details />} />
